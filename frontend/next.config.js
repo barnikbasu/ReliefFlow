011d58f76aa4c581d82c5b8590e19284b02a21bf
Barnik Basu <barnikbasu@gmail.com>
2025-12-23 10:55:40 +0530

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // We disable this for hackathons to avoid double-rendering logs in console
+};
+
+module.exports = nextConfig;
