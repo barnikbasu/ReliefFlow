f6e5e623f5dbdb00113c3a5a2616968e4aad4edb
Barnik Basu <barnikbasu@gmail.com>
2025-12-23 10:56:35 +0530

    Create _app.js

---
+++ frontend/pages/_app.js
@@ -0,0 +1,6 @@
+import "@/styles/globals.css";
+
+// This is the root of your frontend application
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
